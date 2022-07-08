55d8ca94e9e37690cf584b8afbe2224654c9555c
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2022-07-08 01:37:46 +0100

    Show password format hint

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const form = document.getElementById("form");
+const password = document.getElementById("pwd");
+const passwordConfirm = document.getElementById("pwd-confirm");
+const warning = document.getElementById("warning");
 
 function checkPassword() {
-  const password = document.getElementById("pwd");
-  const passwordConfirm = document.getElementById("pwd-confirm");
   if (password.value !== "" && passwordConfirm.value !== "") {
-    const warning = document.getElementById("warning");
     if (password.value !== passwordConfirm.value) {
       warning.textContent = "The passwords don't match!";
       return false;
@@ -15,6 +15,22 @@ function checkPassword() {
     }
   }
 }
+
+function showPasswordFormat() {
+  warning.style.color = "#000000";
+  warning.textContent =
+    "Password must contain one uppercase, one number, minimum 8 characters.";
+}
+
+function hidePasswordFormat() {
+  warning.style.color = "#ff0000";
+  warning.textContent = "";
+}
+
+password.addEventListener("focus", showPasswordFormat);
+passwordConfirm.addEventListener("focus", showPasswordFormat);
+password.addEventListener("blur", hidePasswordFormat);
+passwordConfirm.addEventListener("blur", hidePasswordFormat);
 
 function handleForm(event) {
   if (!checkPassword()) event.preventDefault();
